0d01bac67d746de9840c7e51f7215e69e6cf21ae
Rhonur <onur.demirkale@outlook.com>
2023-08-07 00:07:09 +0300

    update input source factory to include http file input source

--- pkg/inputsource/factory.go
+++ pkg/inputsource/factory.go
@@ -1,9 +1,12 @@
 package inputsource
 
-import "fmt"
+import (
+	"fmt"
+	"mime/multipart"
+)
 
 type InputSourceFactoryInterface interface {
-	CreateInputSource(inputType string, inputData string) (InputSource, error)
+	CreateInputSource(inputType string, inputData interface{}) (InputSource, error)
 }
 
 type InputSourceFactory struct{}
@@ -12,10 +15,12 @@ func NewInputSourceFactory() InputSourceFactoryInterface {
 	return &InputSourceFactory{}
 }
 
-func (f *InputSourceFactory) CreateInputSource(inputType string, inputData string) (InputSource, error) {
+func (f *InputSourceFactory) CreateInputSource(inputType string, inputData interface{}) (InputSource, error) {
 	switch inputType {
 	case "http":
-		return NewHttpBodyInputSource(inputData), nil
+		return NewHttpBodyInputSource(inputData.(string)), nil
+	case "http_file":
+		return NewHttpFileInputSource(inputData.(multipart.File)), nil
 	default:
 		return nil, fmt.Errorf("unknown input type: %s", inputType)
 	}
