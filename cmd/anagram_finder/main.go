c14546a9a89ddd038c0b21d3931b7d9efb351970
Rhonur <onur.demirkale@outlook.com>
2023-08-06 03:26:16 +0300

    update main

--- cmd/anagram_finder/main.go
+++ cmd/anagram_finder/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/onurdemirkale/anagram-finder/api"
+	"github.com/onurdemirkale/anagram-finder/pkg/anagram"
+	"github.com/onurdemirkale/anagram-finder/pkg/inputsource"
+)
+
+func main() {
+	var isf inputsource.InputSourceFactoryInterface = inputsource.NewInputSourceFactory()
+	aff := anagram.NewAnagramFinderFactory()
+	handler := api.NewAnagramHandler(isf, aff)
+
+	http.HandleFunc("/anagram", handler.FindAnagrams)
+	http.ListenAndServe(":8080", nil)
+
+}
