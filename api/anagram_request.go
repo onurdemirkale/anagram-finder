56c255ea9927f178d649fdcf33812750e0b744cd
Rhonur <onur.demirkale@outlook.com>
2023-08-07 23:36:16 +0300

    refactor anagram request, remove unused error constants

--- api/anagram_request.go
+++ api/anagram_request.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"errors"
-	"fmt"
 	"strings"
 )
 
@@ -12,48 +11,63 @@ const (
 	algorithmBasic = "basic"
 )
 
-var (
-	supportedInputTypes     = []string{"http", "http_file"}
-	supportedAlgorithms     = []string{"basic"}
-	errInvalidInput         = errors.New("invalid input provided")
-	errInvalidInputType     = fmt.Errorf("invalid input type. supported types: %s", strings.Join(supportedInputTypes, ", "))
-	errInvalidAlgorithmType = fmt.Errorf("invalid algorithm type. supported algorithms: %s", strings.Join(supportedAlgorithms, ", "))
-	errInvalidFileInput     = errors.New("input data should be empty for file input type")
-)
-
 type AnagramRequest struct {
 	InputType string `json:"inputType"`
 	InputData string `json:"inputData"`
 	Algorithm string `json:"algorithm"`
 }
 
-func (req *AnagramRequest) Validate() error {
-	supportedAlgorithms := map[string]bool{
-		algorithmBasic: true,
+func (req *AnagramRequest) validate() error {
+	if err := req.validateInputType(); err != nil {
+		return err
 	}
 
+	if err := req.validateAlgorithm(); err != nil {
+		return err
+	}
+
+	if err := req.validateInputData(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (req *AnagramRequest) validateInputType() error {
 	supportedTypes := map[string]bool{
 		inputTypeData: true,
 		inputTypeFile: true,
 	}
 
 	if !supportedTypes[req.InputType] {
-		return errInvalidInputType
+		return errors.New(ErrInvalidInputType)
+	}
+
+	return nil
+}
+
+func (req *AnagramRequest) validateAlgorithm() error {
+	supportedAlgorithms := map[string]bool{
+		algorithmBasic: true,
 	}
 
 	if !supportedAlgorithms[req.Algorithm] {
-		return errInvalidAlgorithmType
+		return errors.New(ErrInvalidAlgorithmType)
 	}
 
+	return nil
+}
+
+func (req *AnagramRequest) validateInputData() error {
 	// todo: improve file and http body input validations
 	switch req.InputType {
 	case inputTypeData:
-		if !strings.Contains(req.InputData, ",") {
-			return errInvalidInput
+		if req.InputData == "" || len(strings.Split(req.InputData, ",")) < 2 {
+			return errors.New(ErrInvalidInput)
 		}
 	case inputTypeFile:
 		if req.InputData != "" {
-			return errInvalidFileInput
+			return errors.New(ErrInvalidFileInput)
 		}
 	}
 
